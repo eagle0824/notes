9a7e20c03858c42c4d430bc00198996ddd872305
eagle <l22k77@163.com>
2018-11-05 21:01:45 +0800

    update notes remote not used sh

--- cscope.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find \( -path './out' -o -path './.repo' -o -path './tools' \) -prune -o -type f -a \( -name "*.h" -o -name "*.c" -o -name "*.java" -o -name "*.mk" -o -name "*.xml" -o -name "*.cpp" -o -name "*.sh" \) -print > cscope.files
-cscope -bq
--- cscope_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-find ./ ../../../frameworks/base ../../../libcore/luni/src/main -type f -a \( -name "*.h" -o -name "*.c" -o -name "*.java" -o -name "*.mk" -o -name "*.xml" -o -name "*.cpp" -o -name "*.sh" \) -print > cscope.files
-cscope -bq
